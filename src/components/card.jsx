f231b07865b9459badb4996dcfca339cb1adc793
Tobias <t.cscott2023@gmail.com>
2021-02-25 13:00:00 -0500

    Style Card Component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,6 +8,19 @@ export default function Card() {
     useEffect(() =>{
         fetch('/api/v1/characters').then((res) => res.json()).then(res => {setCharacters(res)}) 
     }, [])
+    // Need to fetch from two separate api endpoints One: Character APi and Two: Episodes within Character Api
+
+    const container = {
+        title: {
+            textAlign: 'center',
+            color: "#2d6cdf"
+        }, 
+
+        img: {
+            borderRadius: '4px',
+            border: '1px solid #F4F4F4'
+        }
+    }
 
     return (
         <>
@@ -15,9 +28,16 @@ export default function Card() {
 
                 return (
                     <Tile key={char.id} style={{borderRadius: '4px', marginTop:'15px'}}>
-                        <h2> {char.name} </h2>
-                        <img alt="characters" src={char.image} style={{borderRadius: '50%'}}/>
+                        <h2 style={container.title}> {char.name} </h2>
+                        <img alt="characters" src={char.image} style={container.img}/>
                         <p style={{borderRadius: '4px'}}> {char.location.name}  </p>
+                        <UnorderedList>
+                            <ListItem>
+                                <p>Status: {char.status}</p>
+                                <p>Species: {char.species}</p>
+                                <p>Gender: {char.gender}</p>
+                            </ListItem>
+                        </UnorderedList>
                     
                         <ModalWrapper
                              buttonTriggerText="See Details"
@@ -26,13 +46,7 @@ export default function Card() {
                              passiveModal
                              danger size='sm'
                         >
-                            <UnorderedList>
-                            <ListItem>
-                                <p>Status: {char.status}</p>
-                                <p>Species: {char.species}</p>
-                                <p>Gender: {char.gender}</p>
-                            </ListItem>
-                        </UnorderedList>
+                            <p>Episodes appeared in will go here </p>
                         </ModalWrapper>
                     </Tile>
                     )
